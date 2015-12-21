152272124550dd15e511efbadd4ce2966618e241
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2015-12-21 16:20:30 -0200

    Update dia 1

---
+++ compile.sh
@@ -0,0 +1,7 @@
+pdflatex -synctex=1 -interaction=nonstopmode --src-specials main
+bibtex main
+pdflatex -synctex=1 -interaction=nonstopmode --src-specials main
+#for file in main.aux main.bbl main.blg main.log main.lof main.out main.toc
+#do
+#  mv $file tmp/
+#done
